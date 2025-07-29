051a7c0e731137d0f00dddd9bf0fbdb926485155
Mahan Lotfi <mahanlotfi85@gmail.com>
2025-07-29 20:42:40 +0430

    feat(api): create services layer to handle API requests.

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react" ; 
-
+import { getCoinList } from "../../services/cryptoApi.js";
 import TableCoin from "../modules/TableCoin.jsx";
 
 function HomePage() {
     const [coins , setCoins] = useState([]) ;
 
     useEffect(()=> { 
-        fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&x_cg_demo_api_key=CG-T7qSqbKk6PFQqRco9ycfznnn")    
-        .then((res) => res.json())
-        .then((json) =>setCoins(json)) ; 
+        const getData = async () => { 
+            const response = await fetch(getCoinList()) ; 
+            const json = await response.json() ; 
+            setCoins(json) ; 
+        }
+        getData() ; 
     } , []) ; 
 
   return (
