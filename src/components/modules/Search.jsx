d272ca8298b8d9f597d45a48e899cea6e571207b
Mahan Lotfi <mahanlotfi85@gmail.com>
2025-07-31 14:03:14 +0430

    style(ui): apply custom styles to search input box.

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { searchCoin } from '../../services/cryptoApi';
 import { RotatingLines } from 'react-loader-spinner';
+import styles from "./Search.module.css"
 
 function Search({currency , setCurrency}) {
     const [text , setText] = useState("") ; 
@@ -43,14 +44,14 @@ function Search({currency , setCurrency}) {
 
     } , [text] )
   return (
-    <div>
+    <div className={styles.searchBox} >
         <input type="text" placeholder="Search" value={text} onChange={e => setText(e.target.value)} />
         <select value={currency} onChange={e => setCurrency(e.target.value)}  >
             <option value="usd">USD</option>
             <option value="eur">EUR</option>
             <option value="jpy">JPY</option>
         </select>
-        <div>
+        <div className={styles.searchResult} >
             {isLoading && <RotatingLines width="50px" height="50px" strokeWidth="2" strokeColor="#3874ff" />}
             <ul>
                 {coins.map((coin) => (<li key={coin.id} >
