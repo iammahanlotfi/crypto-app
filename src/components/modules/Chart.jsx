138a78974aa62f80fc15e8a7106b10398b66a913
Mahan Lotfi <mahanlotfi85@gmail.com>
2025-08-01 09:50:00 +0430

    refactor(chart): format API data for chart compatibility.

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
+import { convertData } from "../../helpers/convertData.js"
 import styles from "./Chart.module.css"
 function Chart({chart , setChart}) {
+    const [type , setType] = useState("prices") ; 
+    console.log(convertData(chart , type)) ; 
   return (
     <div className={styles.container} >
         <span onClick={() => setChart(null)} className={styles.cross} >X</span>
         <div className={styles.chart} >
-        
+
         </div>
     </div>
   )
