fc130d27e37cd778eccf452943116348d9601195
Mahan Lotfi <mahanlotfi85@gmail.com>
2025-08-01 11:20:55 +0430

    feat(chart): render coin chart using formatted data.

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,17 +1,35 @@
 import { useState } from "react";
 import { convertData } from "../../helpers/convertData.js"
 import styles from "./Chart.module.css"
+import { CartesianGrid, LineChart, ResponsiveContainer , Line, YAxis, XAxis, Legend, Tooltip } from "recharts";
 function Chart({chart , setChart}) {
     const [type , setType] = useState("prices") ; 
-    console.log(convertData(chart , type)) ; 
+     
   return (
     <div className={styles.container} >
         <span onClick={() => setChart(null)} className={styles.cross} >X</span>
         <div className={styles.chart} >
-
+        <div className={styles.graph}>
+            <ChartComponent data={convertData(chart , type)} type={type} />
+        </div>
         </div>
     </div>
   )
 }
 
-export default Chart
+export default Chart;
+
+const ChartComponent = ({data , type}) =>{ 
+    return (
+        <ResponsiveContainer width="100%" height="100%" >
+            <LineChart width={400} height={400} data={data} >
+            <Line type="monotone" dataKey={type} stroke="#3874ff" strokeWidth="2px" ></Line>
+            <CartesianGrid stroke="#404042"/>
+            <YAxis dataKey={type} domain={["auto" , "auto"]} />
+            <XAxis dataKey="data"  hide />
+            <Legend/>
+            <Tooltip/>
+            </LineChart>
+        </ResponsiveContainer>
+    )
+}
